548fdaf1db1eb33ef9d07809588c17c689cdc749
toshiki.isokawa <toshi.99.0225@icloud.com>
2021-07-17 00:39:47 +0000

    CommentController#destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,9 +13,21 @@ class CommentsController < ApplicationController
         end
     end
     
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = current_user.comments.find_by(id: params[:id])
+        if @comment.user = current_user
+            @comment.destroy
+            redirect_to post_path(@post)
+        else
+            redirect_to post_path(@post)
+        end
+    end
+    
     private
     
     def comment_params
         params.require(:comment).permit(:comment_content, :post_id)
     end
+    
 end
